3406014b26e12d4897e69c6fb673269d3be38365
AbunguRichard <richardbray.abungu@gmail.com>
2025-12-09 09:02:45 -0500

    Remove old frontend directory (not from Lovable)

--- frontend/src/pages/GrowPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
-import Grow from '../components/Grow';
-
-export default function GrowPage() {
-  const { user } = useContext(AuthContext);
-  
-  return <Grow userId={user?.id || 'demo-user-123'} />;
-}
--- frontend/src/pages/SMSDashboardPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SMSDashboard from '../components/SMSDashboard';
-
-export default function SMSDashboardPage() {
-  return <SMSDashboard />;
-}
